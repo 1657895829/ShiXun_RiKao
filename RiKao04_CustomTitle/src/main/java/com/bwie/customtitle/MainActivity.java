aa6ae63fed00aea62c62c2c29b4852b85cf20ab2
1657895829 <1657895829@qq.com>
2017-11-22 18:56:28 +0800

    自定义组合控件实现标题栏

---
+++ RiKao04_CustomTitle/src/main/java/com/bwie/customtitle/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bwie.customtitle;
+import android.os.Bundle;
+/**
+ * MainActivity中调用时直接 extends TitleActivity，使用在TitleActivity中定义的方法
+ */
+public class MainActivity extends TitleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("标题栏");
+        showBackwardView(R.string.text_back,true);
+        showForwardView(R.string.text_forward,true);
+    }
+}
